dd310207f28e4193507e728162358a88502877d4
alejandro Brand <alejandrobrand8@gmail.com>
2020-11-14 23:45:49 -0800

    data rendered on the 5 day forecast

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function search(event){
     var city = $("#cities").val()
     
     var currentDay = moment().format("MM/DD/YYYY");
-    console.log(currentDay);
+    // console.log(currentDay);
     $(".main_date").text(city + " "+currentDay);
     
     // search(event);
@@ -25,6 +25,8 @@ function request(cityName) {
         $(".data1").text("Temperature: " + response.main.temp + "°F");
         $(".data2").text("Humidity: " + response.main.humidity + "%");
         $(".data3").text("Wind Speed: " + response.wind.speed + "mph");
+        var iconMain = response.weather[0].icon;
+        $("#icon_main").attr("src","http://openweathermap.org/img/w/" + iconMain + ".png")
         var latitude = response.coord.lat;
         // console.log(latitude);
         var longitude = response.coord.lon;
@@ -45,7 +47,7 @@ function secondRequest(latitude,longitude){
         url: scndQueryURL,
         method: "GET"
     }).then(function(response){
-            console.log(response.value);
+            // console.log(response.value);
             var uvI = response.value;
     
             $(".data4").text("UV Index: " + uvI);
@@ -69,6 +71,16 @@ function thirdRequest(city){
         url: trdQueryURL,
         method: "GET"
     }).then(function(response){
-        console.log(response);
+        // console.log(response.list)
+        
+        for (var i = 1; i<6;i++){
+            $(".date"+ i).text(moment().add(i,"days").startOf("day").format("MM/DD/YYYY"));
+            var icon = response.list[i + 8].weather[0].icon;
+            $("#icon" + i).attr("src","http://openweathermap.org/img/w/" + icon + ".png")
+            $(".temp"+i).text("Temperature: " + response.list[i+8].main.temp + "°F");
+            $(".hum"+ i).text("Humidity: " + response.list[i+8].main.humidity + "%");
+            
+            
+        }
     });
 }
